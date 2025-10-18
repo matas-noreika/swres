a59bc279f26ad40439b09af215552070f7f83b4e
matas-noreika <matassimkus55@gmail.com>
2025-10-18 18:22:08 +0100

    Added int0 example for ca1

--- ca1/ca1.md
+++ ca1/ca1.md
@@ -4,11 +4,25 @@ The following C application is a frequency calculator using Input Capture on tim
 The typical calculations for period T and frequency f are given by the following:
 
 ```math
-T = {1 \over f} = f = {1 \over T}
+T = {1 \over f}; f = {1 \over T}
 ```
 
 Since our application depends on a system clock and our timer may as well have a clock divisor we need to adjust our expression. It then translates to the following:
 
 ```math
-T = {1 \over {n \bullet F_{CPU}}}
+T = {tick \over {F_{CPU} \over n}}; f = { {F_{CPU} \over n} \over tick }
+
+Where:
+
+tick = timer tick count,
+T = period,
+f = frequency,
+F_{CPU} = frequency of cpu clock,
+n = timer clock division
 ```
+
+# Design considerations
+The clock division has an impact on the lowest possible period which you can measure which in turn affects frequency. If a lower clock division is used the possibility of our interrupt service routine out pacing our CPU frequency could lead to undesired affects.
+# Observations
+* When running ca1.hex which implements input capture the max out frequency was ~= 2KHz.
+* When running ca1_int0.hex which implements int0 the max out frequency was ~= 28kHz.
--- ca1/src/ca1_int0.c
+++ ca1/src/ca1_int0.c
@@ -1,6 +1,6 @@
 /*
- * Programmer: Matas Noreika 2025-10-15 12:00
- * Purpose: Frequency counter application using timer 1 on ICP1. Falling edge trigger is used.
+ * Programmer: Matas Noreika 2025-10-18 17:09
+ * Purpose: Prints out the frequency of a input square wave on int0 using timer 1
 */
 
 #include <stdio.h>
@@ -9,11 +9,8 @@
 
 #define F_TIMER 1000000//1MHz
 
-//global variable that is incramented during an interrupt causes by input capture pin ICP1
-volatile uint32_t time1 = 0;
+volatile uint32_t count = 0;
 volatile uint32_t previous_count = 0;
-volatile uint32_t freq;
-volatile uint8_t overflow_count = 0;
 
 void main(void){//start of main method
   
@@ -37,27 +34,32 @@ void main(void){//start of main method
   DDRB |= ( (1<<DDB4));
   PORTB ^= (1<<DDB4);
 
-  //enable interrupts for Input capture
-  TIMSK1 |= ((1<<ICIE1) | (1<<TOIE1));
+  //enable overflow interrupt
+  TIMSK1 |= (1<<TOIE1);
+
+  //enable int0 rising edge interrupt
+  EIMSK |= (1<<0);
+  EICRA |= ((1<<ISC01) | (1<<ISC00));
 
   //starts the timer
   //set clock division for timer ~= 1us per tick
   TCCR1B |= ((0<<CS11) | (1<<CS10) );
-
-  //enable global interrupts
-  sei();
-
-  char output_str[25];
   
+  //enable interrupts
+  sei();
+  
+  char output_str[25];
+
   while(1){//start of program loop
-    
+
     if( previous_count >= F_TIMER){
       //send output string
-      sprintf(output_str, "freq: %u\n",freq);
+      sprintf(output_str, "freq: %u\n",count);
       usart_sendString(0, output_str);
       previous_count = 0;
+      count = 0;
     }
-    
+
   }//end of program loop
 
 }//end of main method
@@ -65,32 +67,15 @@ void main(void){//start of main method
 //interrupt service routine of timer 1 overflow
 ISR(TIMER1_OVF_vect){
   cli();
-  overflow_count++;
   previous_count += 65536;
   sei();
 }
 
-//Interrupt service routine of input capture
-ISR(TIMER1_CAPT_vect){
+//interrupt service routine for int0
+ISR(INT0_vect){
   cli();
-  //toggle debug led
+  count++;
   PORTB ^= (1<<DDB4);
-
-  //check if time1 isn't assigned
-  if(!time1){
-    //set our first time stamp
-    time1 = (overflow_count * 65536) + ICR1;
-    sei();
-    //wait for second interrupt
-    return;
-  }
-  
-  //store our second
-  uint32_t time2 = (overflow_count * 65536) + ICR1;
-
-  freq = F_TIMER/(time2 - time1);
-  overflow_count = 0;
-  time1 = 0;
   sei();
 }
 
